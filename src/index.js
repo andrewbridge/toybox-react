dc1a89a2c1cbc59a506dc3c3d8db545e96bcb3b5
Andrew Bridge <andrew@andrewhbridge.co.uk>
2023-04-02 12:41:15 +0100

    feat: Add in an interactive counter using the DOM interface
    The DOM interface allows for dynamic content to be rendered on screen including
    user interaction like this button to increment the count number shown.
    
    While this is powerful, this simple set of HTML elements takes multiple
    statements to render each item, includes repetition and an imperative code style
    which makes re-using this code and state harder to do.

--- src/index.js
+++ src/index.js
@@ -2,3 +2,18 @@ const helloWorld = document.createElement('h1');
 helloWorld.textContent = 'Hello, World!';
 
 document.getElementById('root').appendChild(helloWorld);
+
+let count = 0;
+const countDisplay = document.createElement('p');
+countDisplay.textContent = `Count: ${count}`;
+
+document.getElementById('root').appendChild(countDisplay);
+
+const countIncrementButton = document.createElement('button');
+countIncrementButton.textContent = 'Increment';
+countIncrementButton.addEventListener('click', () => {
+    count++;
+    countDisplay.textContent = `Count: ${count}`;
+}, false);
+
+document.getElementById('root').appendChild(countIncrementButton);
