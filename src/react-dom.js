e3dd7d2be93b08a14a3b237e7d33a8a3ad019001
Andrew Bridge <andrew@andrewhbridge.co.uk>
2023-04-02 18:27:07 +0100

    feat: Introduce vNode comparison
    One of the often touted benefits of using a virtual DOM approach is that two
    versions of the same tree of elements can be compared to determine what the
    differences are between them and what operations are needed on the real DOM
    in order to get from version A to version B.
    
    Here we introduce the most naive version of this diffing algorithm. The
    `reconcile` function takes a previously rendered and yet to be rendered vNode
    and does simple existence checks on both elements to determine if a creation,
    removal or replacement operation is needed.
    
    To aid in this, we move our previous render code to a function which will
    create a RenderedVNode object, which links a vNode to a DOM element as well
    as handling doing the same for a vNode's children.
    
    In its current state, nothing will change. However, the next commit will
    demonstrate how this allows us to begin reacting to external changes by
    allowing for multiple re-renders.

--- src/react-dom.js
+++ src/react-dom.js
@@ -1,18 +1,63 @@
-/** @typedef {import('./types')} VNode */
+/**
+ * @typedef {import('./types')} VNode
+ * @typedef {{ dom: HTMLElement, vNode: VNode, childRenderedVNodes: Array<RenderedVNode> }} RenderedVNode
+ * */
 
 import { TEXT_ELEMENT } from './react';
 
+let rootRenderedVNode = null;
 /**
  * Renders a vNode in the given parentNode (a DOM element)
  * @param {VNode} vNode The vNode to render
  * @param {HTMLElement} parentNode The DOM element to attach `vNode` to
  */
 export function render(vNode, parentNode) {
+    const prevRenderedVNode = rootRenderedVNode;
+    const nextRenderedVNode = reconcile(parentNode, prevRenderedVNode, vNode);
+    rootRenderedVNode = nextRenderedVNode;
+}
+
+/**
+ * Finds the differences between a previously rendered vNode and a new vNode.
+ * 
+ * In its current form it can create previously unseen vNodes, remove previously
+ * seen vNodes and replace previously seen vNodes with an entirely fresh version
+ * 
+ * @param {HTMLElement} parentDom The DOM element to change
+ * @param {RenderedVNode} renderedVNode The previously rendered vNode to compare with vNode
+ * @param {VNode} vNode The new vNode to compare with renderedVNode
+ * @returns {RenderedVNode | null}
+ */
+function reconcile(parentDom, renderedVNode, vNode) {
+    if (renderedVNode === null) {
+        // Create new element in the DOM
+        const newRenderedVNode = instantiate(vNode);
+        parentDom.appendChild(newRenderedVNode.dom);
+        return newRenderedVNode;
+    } else if (vNode === null) {
+        // Remove a previously rendered element from the DOM
+        parentDom.removeChild(renderedVNode.dom);
+        return null;
+    } else {
+        // Replace a previously rendered element with a brand new DOM element
+        const newRenderedVNode = instantiate(vNode);
+        parentDom.replaceChild(newRenderedVNode.dom, renderedVNode.dom);
+        return newRenderedVNode;
+    }
+}
+
+/**
+ * Creates a RenderedVNode instance and underlying DOM element for the given vNode
+ * 
+ * @param {VNode} vNode The vNode to instantiate
+ * @returns {RenderedVNode}
+ */
+function instantiate(vNode) {
     const { type, props } = vNode;
 
     if (typeof type === 'function' && type.prototype?.isComponentClass === true) {
         const componentInstance = new type();
-        return render(componentInstance.render(), parentNode);
+        return instantiate(componentInstance.render());
     }
 
     const isTextElement = type === TEXT_ELEMENT;
@@ -30,7 +75,11 @@ export function render(vNode, parentNode) {
         dom.addEventListener(eventType, props[name], false);
     });
 
-    props.children.forEach((child) => render(child, dom));
+    /** @type {Array<RenderedVNode>} */
+    const childRenderedVNodes = props.children.map(instantiate);
+    childRenderedVNodes.forEach((child) => {
+        dom.appendChild(child.dom);
+    });
 
-    return parentNode.appendChild(dom);
+    return { dom, vNode, childRenderedVNodes };
 }
