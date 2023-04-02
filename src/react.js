659fe22a7aa60f4a1998fc7dd6e2a50638c91987
Andrew Bridge <andrew@andrewhbridge.co.uk>
2023-04-02 16:47:25 +0100

    refactor: Introduce virtual DOM elements
    As we saw in the previous commit, our previous approach had reached the limits
    of its usefulness. While we could've continued to investigate fixes to the
    issues it presented, determining the difference between one render and another
    would be time consuming and potentially result in unstable or resource hungry
    code.
    
    Let's look at the issues we're facing:
    - We want to render some HTML and then, when user interaction happens, render
      it again with the changes that interaction made
    - Where we have class Components with long-lived state we want to retain that
      state so it persists over multiple interactions
    - We want to be able to re-render based on external effects (such as a URL change)
      and have the ability to add, remove and update HTML elements
    
    Since ~2010, we've seen a variety of approaches to solve these problems.
    React, alongside many other frameworks, popularised the use of the virtual DOM
    as an approach to solving this problem, while also reducing the overhead of
    making hundreds of changes directly via the DOM API, which is comparatively
    slow for comparing changes.
    
    We're going to mimic this approach by refactoring our `createElement` to
    produce a plain object containing the information required to render it and
    its children. The returned object from `createElement` will now look like:
    
    {
        // This allows us to hold a reference to a class component
        type: string | Component,
        // By merging props together with children, parent components can interact with children
        props: {
            // These are normal props, e.g. { className: 'heading' } or { onClick: () => {...} }
            [key: string]: string | Function,
            // Merge children nodes in with other properties
            children: Array<VNode | string | number>
        }
    }
    
    Note that loading the page with this commit WILL NOT WORK. Instead, if you open
    your developer tools console, you'll see each VNode that `createElement` creates
    to get an idea of how components are rendered.

--- src/react.js
+++ src/react.js
@@ -20,47 +20,32 @@ Component.prototype.isComponentClass = true;
 const createTextElement = (value) => document.createTextNode(value);
 
 /**
- * createElement returns a new DOM element based on the type and properties provided
+ * createElement returns a new vDOM element based on the type and properties provided
  * 
  * Properties given to `props` with the prefix "on" will be treated as element event
  * listeners.
- * @param {string | Component | Function} type The type of DOM element, Component class, or function component to create
- * @param {{ [key: string]: string | Function}} props The properties of the DOM element
- * @param {Array<HTMLElement>} children The child HTML Elements to be nested within the component
- * @returns {HTMLElement}
+ * @typedef {{ type: string | Component, props: { [key: string]: string | Function, children: Array<VNode> }}} VNode
+ * @param {string | Component | Function} type The type of vDOM element, Component class, or function component to create
+ * @param {{ [key: string]: string | Function}} props The properties of the vDOM element
+ * @param {Array<VNode | string | number>} children The child vDOM Elements to be nested within the component
+ * @returns {VNode}
  */
 export function createElement(type, props, ...children) {
-    let element;
-    if (typeof type === 'function' && type.prototype?.isComponentClass === true) {
-        const componentInstance = new type();
-        element = componentInstance.render();
-    } else if (typeof type === 'function') {
-        element = type(props);
-    } else {
-        element = document.createElement(type);
+    if (typeof type === 'function' && type.prototype?.isComponentClass !== true) {
+        return type(props || {});
     }
+    
+    const mergedProps = Object.assign({}, props);
 
-    const isEvent = (name) => name.startsWith('on');
-    const isAttribute = (name) => !isEvent(name);
-
-    Object.keys(props).filter(isAttribute).forEach((name) => {
-        element[name] = props[name];
-    });
-
-    Object.keys(props).filter(isEvent).forEach((name) => {
-        const eventType = name.toLowerCase().substring(2);
-        element.addEventListener(eventType, props[name], false);
-    });
-
-    children.flat().forEach((child) => {
-        let domElement = child;
-        if (typeof child !== 'object') {
-            domElement = createTextElement(child);
+    const vNode = {
+        type,
+        props: {
+            ...mergedProps,
+            children: children.flat().map((child) => typeof child === 'object' ? child : createTextElement(child)),
         }
-        element.appendChild(domElement);
-    });
-
-    return element;
+    };
+    console.log(vNode);
+    return vNode;
 }
 
 /**
