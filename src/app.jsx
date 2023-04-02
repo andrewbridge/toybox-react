e7d8e53a9709f32be3022d0ae0036677a5cfa921
Andrew Bridge <andrew@andrewhbridge.co.uk>
2023-04-02 18:43:15 +0100

    feat: Demonstrate reconcile diffing
    In this slightly contrived offshoot, we render a single element with the timestamp shown.
    We render the same component every second, and count the iterations. After 10 iterations,
    we pass a `null` value to remove the element from the page.
    
    In doing so, we demonstrate the three scenarios that our naive `reconcile` method caters
    for, first adding, then updating, and finally removing the DOM element from the page.
    
    We'll revert the changes made in this commit and continue on to use our new diffing
    capabilities to return interactivity to our page.

--- src/app.jsx
+++ src/app.jsx
@@ -2,4 +2,11 @@ import * as React from "./react";
 import * as ReactDOM from './react-dom';
 import { App } from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let interations = 0;
+setInterval(() => {
+    if (interations > 10) {
+        return ReactDOM.render(null, document.getElementById('root'));
+    }
+    ReactDOM.render(<p>{Date.now()}</p>, document.getElementById('root'));
+    interations++;
+}, 1000);
