177bd967f7a08c70c9ce54969fc25a27efbf5bad
Andrew Bridge <andrew@andrewhbridge.co.uk>
2023-04-02 18:49:21 +0100

    revert: Remove reconcile function demonstration and resume rendering the app

--- src/app.jsx
+++ src/app.jsx
@@ -2,11 +2,4 @@ import * as React from "./react";
 import * as ReactDOM from './react-dom';
 import { App } from "./components/App";
 
-let interations = 0;
-setInterval(() => {
-    if (interations > 10) {
-        return ReactDOM.render(null, document.getElementById('root'));
-    }
-    ReactDOM.render(<p>{Date.now()}</p>, document.getElementById('root'));
-    interations++;
-}, 1000);
+ReactDOM.render(<App />, document.getElementById('root'));
