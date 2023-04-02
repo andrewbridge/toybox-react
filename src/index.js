a4d14b5cdd79ee98cd3db4029f62d28a42c26410
Andrew Bridge <andrew@andrewhbridge.co.uk>
2023-04-02 13:11:24 +0100

    feat: Add event support to createElement
    To encapsulate events into our createElement, we'll pretend events are actually
    element properties. Our createElement now checks for properties with the prefix
    "on" and then treats these as events and sets the appropriate event listener up.
    
    We still have some issues though:
    - Rendering still requires multiple calls
    - It is not possible to nest elements for more complex layouts
    - State still lives outside of our element encapsulation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 function createElement(type, props) {
     const element = document.createElement(type);
-    Object.keys(props).forEach((name) => {
+
+    const isEvent = (name) => name.startsWith('on');
+    const isAttribute = (name) => !isEvent(name);
+
+    Object.keys(props).filter(isAttribute).forEach((name) => {
         element[name] = props[name];
     });
+
+    Object.keys(props).filter(isEvent).forEach((name) => {
+        const eventType = name.toLowerCase().substring(2);
+        element.addEventListener(eventType, props[name], false);
+    });
+
     return element;
 }
 
@@ -15,10 +25,12 @@ const helloWorld = createElement('h1', { textContent: 'Hello, World!' });
 let count = 0;
 const countDisplay = createElement('p', { textContent: `Count: ${count}` });
 
-const countIncrementButton = createElement('button', { textContent: 'Increment' });
-countIncrementButton.addEventListener('click', () => {
-    count++;
-    countDisplay.textContent = `Count: ${count}`;
-}, false);
+const countIncrementButton = createElement('button', {
+    textContent: 'Increment',
+    onClick: () => {
+        count++;
+        countDisplay.textContent = `Count: ${count}`;
+    }
+});
 
 [helloWorld, countDisplay, countIncrementButton].forEach((element) => render(element, document.getElementById('root')));
